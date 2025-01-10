8dc016cbfcfb8bf8dccc155e4a58e342ddea80c0
Youynow <126891229+Youynow@users.noreply.github.com>
2025-01-10 13:12:03 +0100

    Update soup.py

--- soup.py
+++ soup.py
@@ -24,7 +24,7 @@ def search():
     for g in soup.find_all('div', class_='tF2Cxc'):
         title = g.find('h3').text if g.find('h3') else 'No title'
         link = g.find('a')['href'] if g.find('a') else 'No link'
-        description = g.find('span', class_='aCOpRe').text if g.find('span', class_='aCOpRe') else 'No description'
+        description = g.find('span', class_='aCOpRe').text if g.find('span', class_='VwiC3b') else 'No description'
         results.append({'title': title, 'link': link, 'description': description})
 
     # Uložení výsledků do CSV
